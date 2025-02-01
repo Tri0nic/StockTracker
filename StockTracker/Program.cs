1cbabbad86264105c90968d4c275d62c4e3fbbb8
Tri0nic <androyd0007@yandex.ru>
2025-02-01 03:12:31 +0300

    feat: change main page

--- StockTracker/Program.cs
+++ StockTracker/Program.cs
@@ -44,6 +44,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Products}/{action=Index}/{id?}");
 
 app.Run();
