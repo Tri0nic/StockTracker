76fbb624429f9b6c5c6a5f09676a024decbfe7d2
Tri0nic <androyd0007@yandex.ru>
2025-03-20 18:03:39 +0300

    refactor: remove config hardcode

--- StockTrackerTests/Database/DatabaseTests.cs
+++ StockTrackerTests/Database/DatabaseTests.cs
@@ -1,35 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Xunit;
-using StockTracker.Data;
+﻿using StockTracker.Data;
+using Microsoft.Extensions.DependencyInjection;
+using StockTrackerTests;
 
 namespace StockTracker.Database.Tests
 {
     [TestClass()]
     public class DatabaseTests
     {
+        private readonly StockTrackerContext _context;
+
+        public DatabaseTests()
+        {
+            var services = new ServiceCollection();
+            TestStartup.ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            _context = serviceProvider.GetRequiredService<StockTrackerContext>();
+        }
+
         [TestMethod()]
         public void Database_ShouldHaveOnlyOneTrackedItemPerShop()
         {
-            // Arrange
-            // TODO: Убрать хардкод
-            var options = new DbContextOptionsBuilder<StockTrackerContext>()
-                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StockTracker.Data;Trusted_Connection=True;MultipleActiveResultSets=true")
-                .Options;
+            // Act
+            var shopsWithMultipleTrackedItems = _context.Product
+                .GroupBy(s => s.Shop)
+                .Where(g => g.Count(s => s.IsTracked) > 1)
+                .Select(g => g.Key)
+                .ToList();
 
-            using (var context = new StockTrackerContext(options))
-            {
-                // Act
-                var shopsWithMultipleTrackedItems = context.Product
-                    .GroupBy(s => s.Shop)
-                    .Where(g => g.Count(s => s.IsTracked) > 1)
-                    .Select(g => g.Key)
-                    .ToList();
-
-                // Assert
-                Xunit.Assert.True(
-                    shopsWithMultipleTrackedItems.Count == 0,
-                    $"Найдены магазины с более чем одним отслеживаемым товаром: {string.Join(", ", shopsWithMultipleTrackedItems)}");
-            }
+            // Assert
+            Xunit.Assert.True(
+                shopsWithMultipleTrackedItems.Count == 0,
+                $"Найдены магазины с более чем одним отслеживаемым товаром: {string.Join(", ", shopsWithMultipleTrackedItems)}");
         }
     }
 }
---
+++ StockTrackerTests/TestStartup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StockTracker.Data;
+
+namespace StockTrackerTests
+{
+    internal class TestStartup
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            services.AddDbContext<StockTrackerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("StockTrackerContext")));
+        }
+    }
+}
